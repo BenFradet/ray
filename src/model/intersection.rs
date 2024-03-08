f29cf856f40a9286960e2ddd119fc4cc437fcf39
benfradet <benjamin.fradet@gmail.com>
2024-03-08 19:13:41 +0100

    intersection hit

--- src/model/intersection.rs
+++ src/model/intersection.rs
@@ -13,3 +13,78 @@ impl Intersection {
         Self { t, object: sphere, }
     }
 }
+
+pub trait IntersectionHit {
+    fn hit(self) -> Option<Intersection>;
+}
+
+impl <I: IntoIterator<Item = Intersection>> IntersectionHit for I {
+    fn hit(self) -> Option<Intersection> {
+        self.into_iter().fold(None, |acc, incoming| {
+            if incoming.t < 0. {
+                acc
+            } else {
+                match acc {
+                    None => Some(incoming),
+                    Some(existing) =>
+                        if incoming.t < existing.t {
+                            Some(incoming)
+                        } else {
+                            acc
+                        },
+                }
+            }
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hit_smallest_non_neg() -> () {
+        let s = Sphere::new();
+        let i1 = Intersection::new(5., s);
+        let i2 = Intersection::new(7., s);
+        let i3 = Intersection::new(-3., s);
+        let i4 = Intersection::new(2., s);
+        let is = vec!(i1, i2, i3, i4);
+        assert_eq!(is.hit(), Some(i4));
+    }
+
+    #[test]
+    fn hit_all_neg() -> () {
+        let s = Sphere::new();
+        let i1 = Intersection::new(-1., s);
+        let i2 = Intersection::new(-2., s);
+        let is = vec!(i1, i2);
+        assert_eq!(is.hit(), None);
+    }
+
+    #[test]
+    fn hit_some_neg() -> () {
+        let s = Sphere::new();
+        let i1 = Intersection::new(-1., s);
+        let i2 = Intersection::new(1., s);
+        let is = vec!(i1, i2);
+        assert_eq!(is.hit(), Some(i2));
+    }
+
+    #[test]
+    fn hit_all_pos() -> () {
+        let s = Sphere::new();
+        let i1 = Intersection::new(1., s);
+        let i2 = Intersection::new(2., s);
+        let is = vec!(i1, i2);
+        assert_eq!(is.hit(), Some(i1));
+    }
+
+    #[test]
+    fn new() -> () {
+        let s = Sphere::new();
+        let i = Intersection::new(0., s);
+        assert_eq!(i.t, 0.);
+        assert_eq!(i.object, s);
+    }
+}
